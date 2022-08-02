89911d8337fa9d02feb9453b4c7f21c2ab80912f
mansi90 <mansiarora90@gmail.com>
2022-08-02 13:20:56 +0530

    updated nodejsdocker.groovy to point to my own docker account

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('mansiarora90/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
